47d230f5a6b8487c05e7c33c14917d4fd0b3fbba
narutopden <narutop49@gmail.com>
2021-03-05 19:03:13 +0800

    My first commit

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {TabView,TabBar } from 'react-native-tab-view'
+import converter from 'convert-units'
+import Constants from 'expo-constants'
 
+
+const measures = converter().measures()
+const MeasureView = ({measure})=><Text>{measure}</Text>
+const unCamelCase = (value) => value.replace(/([A-Z])/g, ' $1')
 export default function App() {
+
+  const [index, setIndex] = useState(0)
+  const [routes] = useState(measures.map(m =>(
+    { key : m, 
+      title : unCamelCase(m)
+    })))
+  const renderScene = ({route}) => {
+    return <MeasureView measure={route.key}/>
+  }
+
+  console.log(measures);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {marginTop: Constants.statusBarHeight}]}>
+      <Text style={styles.title}>Unit Converter</Text>
+      <TabView
+        navigationState={{index, routes}}
+        onIndexChange={setIndex}
+        renderScene={renderScene}
+        initialLayout={{width: Dimensions.get('window').width}}
+        renderTabBar = { (props) => (
+          <TabBar {...props} scrollEnabled tabStyle = {{width: 'auto'}}/>
+        )}>
+        
+      </TabView>
     </View>
   );
 }
@@ -14,8 +42,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    fontWeight:'bold',
+    fontSize: 30,
+    textAlign:'center',
+    textTransform: 'uppercase'
+  }
 });
